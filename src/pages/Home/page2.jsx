f5ef9bd41d6507e3c699a60b53c778b486931ea5
GoswamiTushar <tushargoswami1510@gmail.com>
2023-11-20 23:04:00 +0530

    [WIP] :Second page services section

--- src/pages/Home/page2.jsx
+++ src/pages/Home/page2.jsx
@@ -7,93 +7,73 @@ import SEO from '../../Assets/lottie/seo-3.json'
 import Support from '../../Assets/lottie/support.json'
 import { useInView } from 'react-intersection-observer';
 
+const servicesData = [
+  {
+    animationData: ApplicationAnimation,
+    title: 'Web & App Development',
+    description: 'We build beautiful, responsive & feature-rich websites and applications that solve real customer problems',
+  },
+  {
+    animationData: Cloud,
+    title: 'Cloud Native Solutions',
+    description: 'We craft dynamic and scalable cloud services, seamlessly addressing real-world business needs with precision and innovation.',
+  },
+  {
+    animationData: SEO,
+    title: 'One Stop SEO Solutions',
+    description: 'We optimize your digital presence with strategic SEO solutions. Elevate your online visibility and conquer search engine rankings with our expert optimization services.',
+    animationTransform: 'scale(0.8)'
+  },
+  {
+    animationData: Support,
+    title: 'Unmatched Solution Support',
+    description: 'We stand by you with dedicated support. Our team is here, ready to assist, ensuring your success with prompt solutions and expert guidance.',
+  },
+];
+
 const Page2 = () => {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.9,
+  });
 
-    const [ref, inView] = useInView({
-        triggerOnce: true,
-        threshold: 0.9
-    })
+  return (
+    <motion.div ref={ref}>
+      <div className="page-2">
+        <div className="services flex-item">
+          <AnimatePresence>
+            {inView &&
+              servicesData.map((service, index) => (
+                <motion.div
+                  key={`icon${index + 1}`}
+                  initial={{ opacity: 0, x: -50 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0, x: 50 }}
+                  transition={{ duration: 0.5, delay: 0.2 * index }}
+                >
+                  <div className="tech-info">
+                    <div className="lottie-icon-container">
+                      <Lottie
+                        style={{ width: '60px', transform: service.animationTransform || 'scale(1)' }}
+                        className="lottie-icon"
+                        animationData={service.animationData}
+                        loop={true}
+                      />
+                    </div>
+                    <div className="container">
+                      <h4 className="header">{service.title}</h4>
+                      <p className="info">{service.description}</p>
+                    </div>
+                  </div>
+                </motion.div>
+              ))}
+          </AnimatePresence>
+        </div>
 
-    return (
-        <motion.div ref={ref}>
-            <div className="page-2">
-                <div className="services flex-item">
-                    <AnimatePresence>
-                        {inView &&
-                            <>
-                                <motion.div
-                                    key="icon1"
-                                    initial={{ opacity: 0, x: -50 }}
-                                    animate={{ opacity: 1, x: 0 }}
-                                    exit={{ opacity: 0, x: 50 }}
-                                    transition={{ duration: 0.5, delay: 0.2 }}
-                                    className="lottie-icon-container"
-                                >
-                                    <Lottie
-                                        style={{ width: '50px' }}
-                                        className="lottie-icon"
-                                        animationData={ApplicationAnimation}
-                                        loop={true}
-                                    />
-                                </motion.div>
-
-                                <motion.div
-                                    key="icon1"
-                                    initial={{ opacity: 0, x: -50 }}
-                                    animate={{ opacity: 1, x: 0 }}
-                                    exit={{ opacity: 0, x: 50 }}
-                                    transition={{ duration: 0.5, delay: 0.4 }}
-                                    className="lottie-icon-container"
-                                >
-                                    <Lottie
-                                        style={{ width: '50px' }}
-                                        className="lottie-icon"
-                                        animationData={Cloud}
-                                        loop={true}
-                                    />
-                                </motion.div>
-
-                                <motion.div
-                                    key="icon1"
-                                    initial={{ opacity: 0, x: -50 }}
-                                    animate={{ opacity: 1, x: 0 }}
-                                    exit={{ opacity: 0, x: 50 }}
-                                    transition={{ duration: 0.5, delay: 0.6 }}
-                                    className="lottie-icon-container"
-                                >
-                                    <Lottie
-                                        style={{ width: '50px' }}
-                                        className="lottie-icon"
-                                        animationData={SEO}
-                                        loop={true}
-                                    />
-                                </motion.div>
-
-                                <motion.div
-                                    key="icon1"
-                                    initial={{ opacity: 0, x: -50 }}
-                                    animate={{ opacity: 1, x: 0 }}
-                                    exit={{ opacity: 0, x: 50 }}
-                                    transition={{ duration: 0.5, delay: 0.8 }}
-                                    className="lottie-icon-container"
-                                >
-                                    <Lottie
-                                        style={{ width: '50px', transform: 'scale(1.5)' }}
-                                        className="lottie-icon"
-                                        animationData={Support}
-                                        loop={true}
-                                    />
-                                </motion.div>
-                            </>
-                        }
-
-                    </AnimatePresence>
-                </div>
-
-                <div className="services flex-item">one</div>
-            </div>
-        </motion.div>
-    );
+        <div className="services flex-item">one</div>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Page2;
